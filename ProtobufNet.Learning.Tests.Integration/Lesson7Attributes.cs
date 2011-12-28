52a3a87895add6bc985e6911e2e02b4a6694af35
Javier Gonel <j.gonel@pamediakopes.gr>
2011-12-28 17:45:26 +0200

    More examples with serialization without attributes.

--- ProtobufNet.Learning.Tests.Integration/Lesson7Attributes.cs
+++ ProtobufNet.Learning.Tests.Integration/Lesson7Attributes.cs
@@ -1,24 +1,96 @@
 ﻿using System;
 using NUnit.Framework;
-using ProtoBuf.Meta;
 
 namespace ProtobufNet.Learning.Tests.Integration
 {
     public class Lesson7Attributes:LessonBase
     {
-        [Test,Ignore]
-        public void WithoutAnyAttribute()
+        #region WithoutAnyAttribute_OneByOne
+        [Test]
+        public void WithoutAnyAttribute_OneByOne()
         {
-            ProtobufTypeModel.AutoAddMissingTypes = true;
-            
-            var instance = new Lesson7Test1Class1 {Name="Test1Name"};
+            ProtobufTypeModel
+                .Add(typeof (Lesson7Test1Class1), false)
+                .Add(1, "Test");
+            ProtobufTypeModel
+                .Add(typeof(Lesson7Test1Class2), false)
+                .Add(1, "Name");
+            var instance = new Lesson7Test1Class1
+            {
+                Test = new Lesson7Test1Class2 { Name = "Test1Name" }
+            };
             var result = PassThroughProtobuf(instance);
-            Assert.AreEqual("Test1Name", result.Name);
+            Assert.AreEqual("Test1Name", result.Test.Name);
         }
 
         public class Lesson7Test1Class1
         {
+            public Lesson7Test1Class2 Test { get; set; }
+        }
+
+        public class Lesson7Test1Class2
+        {
             public String Name { get; set; }
         }
+        #endregion
+
+        #region WithoutAnyAttribute_UsingAList
+        [Test]
+        public void WithoutAnyAttribute_Many()
+        {
+            ProtobufTypeModel
+                .Add(typeof(Lesson7Test2Class1), false)
+                .Add(new string[] { "Test" });
+            ProtobufTypeModel
+                .Add(typeof(Lesson7Test2Class2), false)
+                .Add(new string[] { "Name" });
+            var instance = new Lesson7Test2Class1
+            {
+                Test = new Lesson7Test2Class2 { Name = "Test2Name" }
+            };
+            var result = PassThroughProtobuf(instance);
+            Assert.AreEqual("Test2Name", result.Test.Name);
+        }
+
+        public class Lesson7Test2Class1
+        {
+            public Lesson7Test2Class2 Test { get; set; }
+        }
+
+        public class Lesson7Test2Class2
+        {
+            public String Name { get; set; }
+        }
+        #endregion
+
+        #region Without Any Attribute Enum
+        [Test]
+        public void WithoutAnyAttribute_Enum()
+        {
+            ProtobufTypeModel
+                .Add(typeof(Lesson7Test3Class1), false)
+                .Add(new string[] { "Test" });
+            ProtobufTypeModel
+                .Add(typeof (Lesson7Test3Enum), false)
+                .EnumPassthru = true;
+
+            var instance = new Lesson7Test3Class1
+            {
+                Test = Lesson7Test3Enum.Three
+            };
+            var result = PassThroughProtobuf(instance);
+            Assert.AreEqual(Lesson7Test3Enum.Three,result.Test);
+        }
+
+        public class Lesson7Test3Class1
+        {
+            public Lesson7Test3Enum Test { get; set; }
+        }
+
+        public enum Lesson7Test3Enum
+        {
+            One,Two,Three
+        }
+        #endregion
     }
 }
