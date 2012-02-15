21f456ec14dbf19998693edd90203408e112c3eb
Ioannis Karadimas <ikaradimas@hotmail.com>
2012-02-15 17:57:30 +0200

    Added test for nullable enums.

--- ProtobufNet.Learning.Tests.Integration/Lesson4Enums.cs
+++ ProtobufNet.Learning.Tests.Integration/Lesson4Enums.cs
@@ -15,11 +15,23 @@ namespace ProtobufNet.Learning.Tests.Integration
         public void PublicEnum()
         {
             var instance = new Lesson4TestClass1() {Size = TestEnum1.Large};
-
             var result = PassThroughProtobuf(instance);
-
             Assert.AreEqual(TestEnum1.Large,result.Size);
         }
+
+		[Test]
+		public void NullableEnum()
+		{
+			// First pass, null value.
+			var instance = new Lesson4TestClass2();
+			var result = PassThroughProtobuf(instance);
+			Assert.IsNull(result.Size);
+			
+			// Second pass, with value.
+			instance.Size = TestEnum1.ExtraLarge;
+			result = PassThroughProtobuf(instance);
+			Assert.AreEqual(result.Size, TestEnum1.ExtraLarge);
+		}
 
         [ProtoContract]
         public class Lesson4TestClass1
@@ -27,6 +39,13 @@ namespace ProtobufNet.Learning.Tests.Integration
             [ProtoMember(1)]
             public TestEnum1 Size { get; set; }
         }
+
+		[ProtoContract]
+		public class Lesson4TestClass2
+		{
+			[ProtoMember(1)]
+			public TestEnum1? Size { get; set; }
+		}
 
         public enum TestEnum1
         {
